eeba65f103eacccfcbe027226718cde0c0f4a252
Tarun-07252 <185054428+Tarun-07252@users.noreply.github.com>
2026-02-03 08:59:33 +0530

    Add files via upload

---
+++ Newprg1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newprg1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Newprg1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
